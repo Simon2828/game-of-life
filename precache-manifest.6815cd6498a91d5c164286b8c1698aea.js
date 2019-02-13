85d24c8145372faa327a7e39e7fd46379a586510
simon2828 <simon.tanner28@gmail.com>
2019-02-13 19:24:08 +0000

    Updates

---
+++ precache-manifest.6815cd6498a91d5c164286b8c1698aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "9b00e16e05faf9278411",
+    "url": "/game-of-life/static/js/main.9b00e16e.chunk.js"
+  },
+  {
+    "revision": "a0b74364eaba7213bbca",
+    "url": "/game-of-life/static/js/1.a0b74364.chunk.js"
+  },
+  {
+    "revision": "9b00e16e05faf9278411",
+    "url": "/game-of-life/static/css/main.d01e8af9.chunk.css"
+  },
+  {
+    "revision": "ff34783dcdcef61289f7d821d5e7a4b6",
+    "url": "/game-of-life/index.html"
+  }
+];
